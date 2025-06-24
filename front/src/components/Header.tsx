0b8b0325661a662dac5e44a1c5d28350384ab397
Marine Ligny <mligny.dev@gmail.com>
2025-06-24 14:10:12 +0200

    correction projet finit

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,10 +1,12 @@
-
+import { Link, } from 'react-router';
 
 function Header() {
     return (
         
             <header className="header">
-                <img className="header-img" src="./src/logo.png" alt="logo orecipes"/>
+                <Link to="/">
+                    <img className="header-img" src="/src/logo.png" alt="logo orecipes"/>
+                </Link>
                 <form className="form">
                 <input type="text" placeholder="Adresse Email"/>
                 <input type="password" placeholder="Mot de passe"/>
